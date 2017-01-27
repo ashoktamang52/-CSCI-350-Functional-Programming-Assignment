de0aca84e1724cd357ff3737baa82496a5481869
Ashok Tamang <ashoktamang52@gmail.com>
2017-01-27 03:21:25 -0500

    Style code and comments

--- fp.rkt
+++ fp.rkt
@@ -1,15 +1,18 @@
-;; Given a list, the function returns a list with elements in reverse order.
-;; Even the sublists inside the list are reversed within.
-(DEFINE (reverse-general L)
-  ;; Helper function that takes two lists as parameters.
-  ;; Returns a list where the two lists are concatenated with each other.
-  (DEFINE (append L1 L2)
-    (COND
-      ((NULL? L1) L2)
-      (ELSE (CONS (CAR L1)
-                  (append (CDR L1) L2)))
-     )
+;; [Helper function]
+;; Parameter(s): <list> <list>
+;; Returns: List where the two lists are concatenated with each other.
+(DEFINE (append L1 L2)
+  (COND
+    ((NULL? L1) L2)
+    (ELSE (CONS (CAR L1) (append (CDR L1) L2)))
   )
+)
+
+;; [Part 1]
+;; Parameter(s): <list>
+;; Returns: List with elements in reverse order.
+;;          Even the sublists inside the list are reversed within.
+(DEFINE (reverse-general L)
   (COND
     ((NULL? L) '()) ;; Empty List
     ((LIST? (CAR L)) (append (reverse-general (CDR L)) (LIST (reverse-general(CAR L))))) 
@@ -17,7 +20,9 @@
   )
  )
 
-;; Given a list, the function returns the sume of numbers not in nested lists in the list.
+;; [Part 2]
+;; Parameter(s): <list>
+;; Returns: Sum of numbers not in nested lists in the list.
 (DEFINE (sum-up-numbers-simple L)
   (COND
     ((NULL? L) 0)
@@ -26,7 +31,10 @@
   )
 )
 
-;; Given a list, the function returns the sum of all the numbers (including those in nested lists) in the liost.
+;; [Part 3]
+;; Parameter(s): <list>
+;; Returns: Sum of all the numbers (including those in nested lists) in the liost.
+
 (DEFINE (sum-up-numbers-general L)
   (COND
     ((NULL? L) 0)
@@ -36,72 +44,77 @@
   )
 )
 
-;; Helper Funciton: Given a list of numeric atoms, the function returns a minimum of the numbers in that list.
+;; [Helper function]
+;; Parameter(s): <list>
+;; Assumption(s): List only contains numeric atoms.
+;; Returns: Minimum of the numbers in that list.
 (DEFINE (min-from-list L)
-        (COND
-         ((NULL? (CDR L)) (CAR L))
-         (ELSE
-          (IF (NUMBER? (CAR L))
-           (MIN (CAR L) (min-from-list (CDR L)))
-           (min-from-list(CDR L))
-          )
-         )
-        )
+  (COND
+    ((NULL? (CDR L)) (CAR L))
+    (ELSE
+      (IF (NUMBER? (CAR L))
+        (MIN (CAR L) (min-from-list (CDR L)))
+        (min-from-list(CDR L))
+      )
+    )
+  )
 )
 
-;; Helper Function: Given a list of alpha-numeric atoms, the function returns a new list of
-;; only numeric atoms.
+;; [Helper function]
+;; Parameter(s): <list>
+;; Returns: List of only numeric atoms.
 (DEFINE (only-numeric L)
-        (COND
-         ((NULL? L) '())
-         (ELSE
-          (IF (NUMBER? (CAR L))
-              (CONS (CAR L) (only-numeric (CDR L)))
-              (only-numeric (CDR L))
-          )
-         )
-        )
+  (COND
+    ((NULL? L) '())
+    (ELSE
+      (IF (NUMBER? (CAR L))
+        (CONS (CAR L) (only-numeric (CDR L)))
+        (only-numeric (CDR L))
+      )
+    )
+  )
 )
 
-;; Helper Function: Given a list and a number, the function returns a new list of all the numbers
-;; that are greater than the given number
+;; [Helper function]
+;; Parameter(s): <list> <numeric-atom>
+;; Returns: List of all the numbers that are greater than the given number.
 (DEFINE (greater-than-list L N)
-        (COND
-         ((NULL? L) '())
-         (ELSE
-          (IF (> (CAR L) N)
-              (CONS (CAR L) (greater-than-list (CDR L) N))
-              (greater-than-list (CDR L) N)
-          )
-         )
-        )
+  (COND
+    ((NULL? L) '())
+    (ELSE
+      (IF (> (CAR L) N)
+        (CONS (CAR L) (greater-than-list (CDR L) N))
+        (greater-than-list (CDR L) N)
+      )
+    )
+  )
 )
          
-
-;; Given two different lists, the function returns the minimum of numbers in L1 that are
-;; larger than the smallest number in L2.
+;; [Part 4]
+;; Parameter(s): <list> <list>
+;; Returns: Minimum of numbers in L1 that are larger than the smallest number in L2.
 (DEFINE (min-above-min L1 L2)
   (COND
     ((NULL? L1) #F)
     ((NULL? L2)
-     (IF (NULL? (only-numeric L1)) ; If list doesn't have any numeric atoms.
-         #F
-         (min-from-list (only-numeric L1))
-     )
+      (IF (NULL? (only-numeric L1)) ; If list doesn't have any numeric atoms.
+        #F
+        (min-from-list (only-numeric L1))
+      )
     )
     (ELSE
-     (IF (NULL? (only-numeric L1)) ; If L1 doesn't have any numeric atoms.
-          #F
-          (IF (NULL? (only-numeric L2))
-              (min-from-list (only-numeric L1))
-              (IF (NULL? (greater-than-list (only-numeric L1) (min-from-list (only-numeric L2))))
-                  #F
-                  (min-from-list (greater-than-list
+      (IF (NULL? (only-numeric L1)) ; If L1 doesn't have any numeric atoms.
+        #F
+        (IF (NULL? (only-numeric L2))
+          (min-from-list (only-numeric L1))
+          (IF (NULL? (greater-than-list (only-numeric L1) (min-from-list (only-numeric L2))))
+            #F
+            (min-from-list (greater-than-list
                                   (only-numeric L1)
                                   (min-from-list (only-numeric L2))))
-               )
-          ) 
+          )
+        ) 
       )
-     )
+    )
   ) 
 )
